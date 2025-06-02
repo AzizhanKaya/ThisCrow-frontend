3515971b1c2c723fc53d227fe6dbfa5ecf03cd31
AzizhanKaya <kayaazizhan3@gmail.com>
2025-06-02 20:35:04 +0300

    chatting mechanism

--- src/views/ServersList.vue
+++ src/views/ServersList.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 	import { ref } from 'vue';
 	import { getServerList } from '@/api/state';
+	import { Icon } from '@iconify/vue';
 	import Server from '@/components/Server.vue';
 
 	const { servers, error, isFetching } = getServerList();
 </script>
 
 <template>
-	<div v-if="isFetching" class="loading-state">Yükleniyor...</div>
-	<div v-else-if="error" class="error-state">Bir hata oluştu: {{ error }}</div>
+	<div v-if="isFetching" class="loading-state">
+		<Icon icon="line-md:loading-loop" />
+	</div>
+	<div v-else-if="error" class="error-state">
+		<Icon icon="icon-park-solid:error" />
+	</div>
 	<div v-else-if="servers" class="server-list">
 		<Server v-for="server in servers" :key="server.id" :server="server" />
 	</div>
