609982691e498003117f77a8314b61146c27daaf
AzizhanKaya <kayaazizhan3@gmail.com>
2026-03-30 00:31:57 +0300

    workflow changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::Manager;
 use tauri::WebviewUrl;
 use tauri::WebviewWindowBuilder;
 use tauri_plugin_updater::UpdaterExt;
@@ -15,24 +16,49 @@ pub fn run() {
             let handle = app.handle().clone();
 
             tauri::async_runtime::spawn(async move {
-                match handle.updater().unwrap().check().await {
-                    Ok(Some(update)) => {
-                        println!("Update available: {}", update.version);
-                        let _ = WebviewWindowBuilder::new(
-                            &handle,
-                            "updater",
-                            WebviewUrl::App("updater.html".into()),
-                        )
-                        .title("Updater")
-                        .inner_size(400.0, 300.0)
-                        .build()
-                        .expect("Updater window could not be created");
-                    }
-                    Ok(None) => {
-                        println!("Application is already up to date.");
-                    }
+                let main_window = handle.get_webview_window("app");
+
+                match handle.updater() {
+                    Ok(updater) => match updater.check().await {
+                        Ok(Some(update)) => {
+                            println!("Update available: {}", update.version);
+
+                            if let Some(ref win) = main_window {
+                                let _ = win.hide();
+                            }
+
+                            let _ = WebviewWindowBuilder::new(
+                                &handle,
+                                "updater",
+                                WebviewUrl::App("updater.html".into()),
+                            )
+                            .title("ThisCrow Updater")
+                            .inner_size(400.0, 300.0)
+                            .decorations(false)
+                            .transparent(true)
+                            .always_on_top(true)
+                            .center()
+                            .build()
+                            .expect("Updater window could not be created");
+                        }
+                        Ok(None) => {
+                            println!("Application is already up to date.");
+                            if let Some(ref win) = main_window {
+                                let _ = win.show();
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Update check failed: {}", e);
+                            if let Some(ref win) = main_window {
+                                let _ = win.show();
+                            }
+                        }
+                    },
                     Err(e) => {
-                        eprintln!("Update check failed: {}", e);
+                        eprintln!("Updater plugin error: {}", e);
+                        if let Some(ref win) = main_window {
+                            let _ = win.show();
+                        }
                     }
                 }
             });
